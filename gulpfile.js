ef1fb1138372ebe1798ed30c14a6dd78d3b80f06
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-09-19 15:24:01 +0200

    compoentes

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const {src, dest, watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
 
 function css(done) {
-  src('src/scss/app.scss')//identificar archivo de sass
+  src('src/scss/**/*.scss')//identificar archivo de sass
   .pipe(sass())//compilarlo
   .pipe(dest("build/css"));//guardarla e el disco duro
 
   done();//call back que acisa qe llegmos al final de la ejecucion
 }
 function dev(done) {
-  watch('src/scss/app.scss', css)
+  watch('src/scss/**/*.scss', css)
 
   done();
 }
