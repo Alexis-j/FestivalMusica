3b6f27361ab989a33ac714c0d16452cbdadc3b19
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-09-25 09:29:31 +0200

    globles

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const {src, dest, watch } = require("gulp");
 const sass = require('gulp-sass')(require('sass'));
+const plumber = require('gulp-plumber')
 
 function css(done) {
   src('src/scss/**/*.scss')//identificar archivo de sass
+  .pipe(plumber())
   .pipe(sass())//compilarlo
   .pipe(dest("build/css"));//guardarla e el disco duro
 
