1a60e4ece761ea1d4ca1c38b25f0e9b9c48f7c1f
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-09-27 13:54:18 +0200

    img

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber')
 
 //Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css(done) {
@@ -25,6 +27,18 @@ function versionWebp (done) {
   src('src/img/**/*.{png,jpg}')
   .pipe( webp())
   .pipe( dest('build/img'));
+
+  done();
+}
+
+function imagenes(done) {
+  const opciones = {
+    optimizationLevel: 3
+  }
+  src('src/img/**/*.{png,jpg}')
+  .pipe( cache( imagemin(opciones) ) )
+  .pipe(dest('build/img'))
+  done();
 }
 function dev(done) {
   watch('src/scss/**/*.scss', css)
@@ -34,5 +48,6 @@ function dev(done) {
 
 
 exports.css = css;
+exports.imagenes = imagenes
 exports.versionWebp = versionWebp;
-exports.dev = parallel (versionWebp, dev);
+exports.dev = parallel (imagenes, versionWebp, dev);
