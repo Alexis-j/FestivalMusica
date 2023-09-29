a38b048c79d8e4639540cb7dd49b90c7d8c793ba
alexis jimenez castillo <alexis.jcastillo@outlook.com>
2023-09-29 16:54:30 +0200

    foto overlay

--- src/js/galeria.js
+++ src/js/galeria.js
@@ -14,8 +14,29 @@ function crearGaleria() {
     imagen.innerHTML = `
         <source srcset="build/img/thumb/${i}.avif" type="image/avif">
         <source srcset="build/img/thumb/${i}.webp" type="image/webp">
-        <img loading="lazy" width="200" height="300"="build/img/thumb/${i}.avif" alt="Imagen Galeria">`
+        <img loading="lazy" width="200" height="300"="build/img/thumb/${i}.avif" alt="Imagen Galeria">
+        `;
+
+    imagen.onclick = function() {
+      mostrarImagen(i);
+    }
 
     galeria.appendChild(imagen);
   }
+}
+
+function mostrarImagen(id) {
+  const imagen = document.createElement('picture');
+  imagen.innerHTML = `
+      <source srcset="build/img/grande/${id}.avif" type="image/avif">
+      <source srcset="build/img/grande/${id}.webp" type="image/webp">
+      <img loading="lazy" width="200" height="300"="build/img/grande/${id}.avif" alt="Imagen Galeria">
+      `;
+      const overlay = document.createElement('DIV');
+      overlay.appendChild(imagen);
+      overlay.classList.add('overlay');
+
+      const body = document.querySelector('body');
+      body.appendChild(overlay);
+
 }
